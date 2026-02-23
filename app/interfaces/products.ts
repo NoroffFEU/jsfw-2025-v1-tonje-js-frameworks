ad9c8e4f18fa24d020ba8a0c262b29424951fae3
TonjeSchjefstad <tonsch03841@stud.noroff.no>
2026-02-23 16:10:54 +0100

    Add interfaces for product data

---
+++ app/interfaces/products.ts
@@ -0,0 +1,27 @@
+export interface Product {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  discountedPrice: number;
+  image: ProductImage;
+  rating: number;
+  tags: string[];
+  reviews: Review[];
+}
+
+export interface ProductImage {
+    url: string;
+    alt: string;
+}
+
+export interface Review {
+    id: string;
+    username: string;
+    rating: number;
+    description: string;
+}
+
+export interface ApiResponse {
+    data: Product[];
+}
