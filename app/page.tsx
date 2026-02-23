01dd78b2e64065de0beed3a322751d39de88336b
TonjeSchjefstad <tonsch03841@stud.noroff.no>
2026-02-23 15:53:19 +0100

    Add hero section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <main>
-      <h1></h1>
+      <section 
+        className="bg-card px-6 py-20 text-center md:py-32">
+
+        <h1 className="text-4xl font-bold uppercase tracking-tight md:text-6xl">Elevate Every Day</h1>
+        <p className="mx-auto mt-4 max-w-md text-text text-text-secondary">Enjoy variety, quality, and convenience in one modern shopping destination.</p>
+      </section>
     </main>
 
   );
